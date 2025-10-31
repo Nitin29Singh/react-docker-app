6179ef159d8127151aa02520ca8dca496935fde2
Nitin29Singh <nitin2004singhh29@gmail.com>
2025-10-31 15:10:08 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 React + Docker = Production Ready!</h1>
+      <p>This React app is served from an optimized Docker image using Nginx.</p>
+    </div>
+  );
+}
+
+export default App;
